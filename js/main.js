5debaa0d8827237a19ab52f1596284cf285eefc3
Elton Souza <elton.souza1515@gmail.com>
2021-06-09 12:13:38 -0300

    reformulando

--- js/main.js
+++ js/main.js
@@ -1,19 +1,41 @@
-const data = document.querySelectorAll('[data-digito]')
-const op = document.querySelectorAll('[data-operador]')
-
-data.forEach((btn)=>{
-    btn.addEventListener('click',(event)=>{
-        const number = event.target.textContent
-        getData(number)
-    })
-})
-op.forEach((operador)=>{
-    operador.addEventListener('click',(event)=>{
-        const operador = event.target.textContent
-        getData(operador)
-    })
-})
-
 function getData(event){
-    console.log(event)
+    event.preventDefault()
+
+    const camp1 = document.querySelector('[data-number1]')
+    const camp2 = document.querySelector('[data-number2]')
+    const op = document.querySelector('[data-operador]')
+    
+    const n1 = parseFloat(camp1.value) 
+    const n2 = parseFloat(camp2.value)
+    const oper = op.value
+
+    calc(n1,n2,oper)
+
+    camp1.value=''
+    camp2.value=''
+}
+function calc(n1,n2,oper){
+    const result = document.querySelector('[data-result]')
+
+    switch(oper){
+        case "somar":
+            const sm = n1 + n2
+            result.value = sm
+            console.log(sm)
+            break
+
+        case "subtrair":
+            const sub = n1 - n2
+            result.value = sub
+            break
+
+        case "multiplicar":
+            const mult = n1*n2
+            result.value = mult
+            break
+
+        case "dividir":
+            const div = n1/n2
+            result.value = div
+    }
 }
