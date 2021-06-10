6d65329e0eddd58502f337fb094f21b4b0863ef4
Elton Souza <elton.souza1515@gmail.com>
2021-06-10 15:38:06 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -8,36 +8,40 @@ function getData(event){
     const n1 = parseFloat(camp1.value) 
     const n2 = parseFloat(camp2.value)
     const oper = op.value
-
+    
     calc(n1,n2,oper)
 
     camp1.value=''
     camp2.value=''
 }
 function calc(n1,n2,oper){
-    if(n1 && n2 == NaN){
-        alert("Opa")
+    if(n1 == NaN && n2 == NaN){
+        console.log("Opa")
     }
-    const result = document.querySelector('[data-result]')
 
     switch(oper){
         case "somar":
             const sm = n1 + n2
-            result.value = sm
+            render(sm)
             break
 
         case "subtrair":
             const sub = n1 - n2
-            result.value = sub
+                render(sub)
             break
 
         case "multiplicar":
             const mult = n1*n2
-            result.value = mult
+            render(mult)
             break
 
         case "dividir":
             const div = n1/n2
-            result.value = div
+            render(div)
     }
+}
+
+function render(valor){
+    const result = document.querySelector('[data-result]')
+    result.value = valor
 }
