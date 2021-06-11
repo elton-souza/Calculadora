a04bf949dac3857c98c56a305fc09e1e896240cd
Elton Souza <elton.souza1515@gmail.com>
2021-06-11 17:51:44 -0300

    Update main.js

--- Calculadora 2/js/main.js
+++ Calculadora 2/js/main.js
@@ -1,15 +1,23 @@
+const painel = document.querySelector('[data-painel]')
+
 const arrayNumber = document.querySelectorAll('[data-number]')
 arrayNumber.forEach((valor)=>{
-    valor.addEventListener('click', ()=>{getData(valor.value)} )
+    valor.addEventListener('click', ()=>{
+        console.log(valor.value)
+        getData(valor.value)} )
 })
-
 
 function getData(valor){
     const number = valor
     render(number)
 }
-
 function render(number){
-    const painel = document.querySelector('[data-painel]')
     painel.value += number
+}
+function clear(){
+    painel.value = ""
+}
+
+function result(valor1,valor2,op){
+
 }
